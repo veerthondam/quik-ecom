3405798aa711ba1ba63848f32c52f773d85f0d31
veerthondam <veerthondam@gmail.com>
2024-12-04 21:45:12 +0530

    added cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
 
 // Path to the JSON file
 const dbPath = path.join(__dirname, "db.json");
